47bc89662d9cb57f84756e879d894670eb6923cd
Braian Corrêa <00.braian.dev@gmail.com>
2022-09-02 13:26:29 -0300

    Keyboard Input

--- main.lua
+++ main.lua
@@ -7,25 +7,51 @@ function love.load()
     pacman.x = 200
     pacman.y = 250
     pacman.eat = true
+    pacman.angle1 = 1
+    pacman.angle2 = 5
 
-    _G.food_x = 600
-    _G.food_eaten = false
+    _G.food ={
+        x = 600,
+        eaten = false
+    }
 end
 
 function love.update(dt)
-    pacman.x = pacman.x + 1
+    -- Move on X and Y
+    if love.keyboard.isDown("a") then
+        pacman.x = pacman.x - 1
+    end
+    if love.keyboard.isDown("d") then
+        pacman.x = pacman.x + 1
+    end
+    if love.keyboard.isDown("w") then
+        pacman.y = pacman.y - 1
+    end
+    if love.keyboard.isDown("s") then
+        pacman.y = pacman.y + 1
+    end
 
-    if pacman.x >= food_x + 20 then
-        _G.food_eaten = true
+    -- Rotate
+    if love.keyboard.isDown("e") then
+        pacman.angle1 = pacman.angle1 + math.pi * dt
+        pacman.angle2 = pacman.angle2 + math.pi * dt
+    end
+    if love.keyboard.isDown("q") then
+        pacman.angle1 = pacman.angle1 - math.pi * dt
+        pacman.angle2 = pacman.angle2 - math.pi * dt
+    end
+
+    if pacman.x >= food.x + 20 then
+        food.eaten = true
     end
 end
 
 function love.draw()
-    if not food_eaten then
+    if not food.eaten then
         love.graphics.setColor(0, 0, 0)
-        love.graphics.rectangle("fill", food_x, 200, 70, 70)
+        love.graphics.rectangle("fill", food.x, 200, 70, 70)
     end
 
     love.graphics.setColor(1, 0.7, 0.1)
-    love.graphics.arc("fill", pacman.x, pacman.y, 60, 1, 5)
+    love.graphics.arc("fill", pacman.x, pacman.y, 60, pacman.angle1, pacman.angle2)
 end
